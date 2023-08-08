bab374a42e3179c183e52f7026ccaa50c485e530
Nithishluhar <nithishluhar2606@gmail.com>
2023-08-08 20:51:53 +0530

    my first react app

---
+++ React task 01/src/Price.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+
+function Price() {
+  return React.createElement('div',{className:'parent'},
+  
+  React.createElement('div',null,
+  React.createElement('div',{className:'card'},
+  React.createElement('h4',null,"FREE"),
+  React.createElement('h1',null,"$0",React.createElement('span',{className:"span1"},"/month")),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Single user"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"5GB Storage"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"}))," Unlimited Public Projects "),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Community Access"),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Unlimited  Public Projects"),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Dedicated Phone Support"),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Free Subdomain "),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Monthly Status Reports")),
+  React.createElement('button',{className:"bnt"},"BUTTON"))),
+ 
+  React.createElement('div',null,
+  React.createElement('div',{className:'card'},
+  React.createElement('h4',null,"PLUS"),
+  React.createElement('h1',null,"$9",React.createElement('span',{className:"span1"},"/month")),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Single user"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"5GB Storage"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"}))," Unlimited Public Projects "),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Community Access"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Unlimited  Public Projects"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Dedicated Phone Support"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Free Subdomain "),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Monthly Status Reports")),
+  React.createElement('button',{className:"bnt"},"BUTTON"))),
+
+  React.createElement('div',null,
+  React.createElement('div',{className:'card'},
+  React.createElement('h4',null,"PRO"),
+  React.createElement('h1',null,"$49",React.createElement('span',{className:"span1"},"/month")),
+  React.createElement('hr',null),
+  React.createElement('ul',{className:'fa-ul'},
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Single user"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"5GB Storage"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"}))," Unlimited Public Projects "),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Community Access"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Unlimited  Public Projects"),
+  React.createElement('li',null,React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fa fa-check"})),"Dedicated Phone Support"),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Free Subdomain "),
+  React.createElement('li',{className:"text-muted"},React.createElement('span',{className:'fa-li'},React.createElement('i',{className:"fas fa-times"})),"Monthly Status Reports")),
+  React.createElement('button',{className:"bnt"},"BUTTON"))),
+  )
+  
+}
+
+
+export default Price
---
+++ React task 01/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Price from './price.jsx'
+import './Price.css'
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <Price></Price>
+ 
+  </React.StrictMode>,
+)
